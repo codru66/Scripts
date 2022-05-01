dcc827050fce7c7eda48a06fc7b99447db5314bb
codru66 <102724087+codru66@users.noreply.github.com>
2022-05-01 08:16:02 +0200

    Update allScript.sh

--- allScript.sh
+++ allScript.sh
@@ -8,13 +8,11 @@ apt install nginx -y
 apt install mariadb-server -y
 apt install php-fpm php-mysql -y
 apt install timeshift -y
-apt install python -y
 apt install fail2ban -y
 apt install preload -y
 apt install bleachbit -y
 apt install pulseaudio -y
 apt install wget -y
-apt install terminator -y
 apt install git -y
 apt install grub-customizer -y
 apt install npm -y
